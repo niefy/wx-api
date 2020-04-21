66cb15de30e0ae8b0a246709a7e386ea10eab390
niefy <34568539+niefy@users.noreply.github.com>
2020-04-21 17:19:05 +0800

    移除字符集排序规则

--- db/V0.4以前版本数据库升级脚本.sql
+++ db/V0.4以前版本数据库升级脚本.sql
@@ -7,7 +7,7 @@
     3. 更新微信粉丝用户表（wx_user），会清空数据（可以在管理后台再次同步）
  如使用本脚本升级后系统出现问题，请备份自己的重要数据后，执行最新的数据库脚本（重置整个数据库）
 
- Date: 2020/4/17
+ Date: 2020/4/21
  Author: Nifury
 */
 -- ----------------------------
@@ -16,12 +16,12 @@
 DROP TABLE IF EXISTS `msg_template`;
 CREATE TABLE `msg_template`  (
   `id` bigint(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `template_id` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '公众号模板ID',
-  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '模版名称',
-  `title` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标题',
-  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '模板内容',
+  `template_id` varchar(100)  NOT NULL COMMENT '公众号模板ID',
+  `name` varchar(50)  NULL DEFAULT NULL COMMENT '模版名称',
+  `title` varchar(20)  NULL DEFAULT NULL COMMENT '标题',
+  `content` text  NULL COMMENT '模板内容',
   `data` json NULL COMMENT '消息内容',
-  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '链接',
+  `url` varchar(255)  NULL DEFAULT NULL COMMENT '链接',
   `miniprogram` json NULL COMMENT '小程序信息',
   `status` tinyint(1) UNSIGNED NOT NULL COMMENT '是否有效',
   `update_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
@@ -37,11 +37,11 @@ DROP TABLE IF EXISTS `sys_menu`;
 CREATE TABLE `sys_menu`  (
   `menu_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `parent_id` bigint(20) NULL DEFAULT NULL COMMENT '父菜单ID，一级菜单为0',
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '菜单名称',
-  `url` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '菜单URL',
-  `perms` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '授权(多个用逗号分隔，如：user:list,user:create)',
+  `name` varchar(50)  NULL DEFAULT NULL COMMENT '菜单名称',
+  `url` varchar(200)  NULL DEFAULT NULL COMMENT '菜单URL',
+  `perms` varchar(500)  NULL DEFAULT NULL COMMENT '授权(多个用逗号分隔，如：user:list,user:create)',
   `type` int(11) NULL DEFAULT NULL COMMENT '类型   0：目录   1：菜单   2：按钮',
-  `icon` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '菜单图标',
+  `icon` varchar(50)  NULL DEFAULT NULL COMMENT '菜单图标',
   `order_num` int(11) NULL DEFAULT NULL COMMENT '排序',
   PRIMARY KEY (`menu_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 111 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '菜单管理' ROW_FORMAT = Dynamic;
@@ -111,13 +111,13 @@ INSERT INTO `sys_menu` VALUES (1111, 108, '同步', NULL, 'wx:wxuser:save', 2, N
 DROP TABLE IF EXISTS `template_msg_log`;
 CREATE TABLE `template_msg_log`  (
   `log_id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `touser` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户openid',
-  `template_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'templateid',
+  `touser` varchar(50)  NULL DEFAULT NULL COMMENT '用户openid',
+  `template_id` varchar(50)  NULL DEFAULT NULL COMMENT 'templateid',
   `data` json NULL COMMENT '消息数据',
-  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '消息链接',
+  `url` varchar(255)  NULL DEFAULT NULL COMMENT '消息链接',
   `miniprogram` json NULL COMMENT '小程序信息',
   `send_time` datetime(0) NULL DEFAULT NULL COMMENT '发送时间',
-  `send_result` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '发送结果',
+  `send_result` varchar(255)  NULL DEFAULT NULL COMMENT '发送结果',
   PRIMARY KEY (`log_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 116249 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '微信模版消息发送记录' ROW_FORMAT = Dynamic;
 
@@ -126,20 +126,20 @@ CREATE TABLE `template_msg_log`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `wx_user`;
 CREATE TABLE `wx_user`  (
-  `openid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '微信openid',
-  `phone` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '手机号',
-  `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '昵称',
+  `openid` varchar(50)  NOT NULL COMMENT '微信openid',
+  `phone` char(11)  NULL DEFAULT NULL COMMENT '手机号',
+  `nickname` varchar(50)  NULL DEFAULT NULL COMMENT '昵称',
   `sex` tinyint(4) NULL DEFAULT NULL COMMENT '性别(0-未知、1-男、2-女)',
-  `city` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '城市',
-  `province` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '省份',
-  `headimgurl` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '头像',
+  `city` varchar(20)  NULL DEFAULT NULL COMMENT '城市',
+  `province` varchar(20)  NULL DEFAULT NULL COMMENT '省份',
+  `headimgurl` varchar(255)  NULL DEFAULT NULL COMMENT '头像',
   `subscribe_time` datetime(0) NULL DEFAULT NULL COMMENT '订阅时间',
   `subscribe` tinyint(3) UNSIGNED NULL DEFAULT 1 COMMENT '是否关注',
-  `unionid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT 'unionid',
-  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '备注',
+  `unionid` varchar(50)  NULL DEFAULT NULL COMMENT 'unionid',
+  `remark` varchar(255)  NULL DEFAULT NULL COMMENT '备注',
   `tagid_list` json NULL COMMENT '标签ID列表',
-  `subscribe_scene` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '关注场景',
-  `qr_scene_str` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '扫码场景值',
+  `subscribe_scene` varchar(50)  NULL DEFAULT NULL COMMENT '关注场景',
+  `qr_scene_str` varchar(64)  NULL DEFAULT NULL COMMENT '扫码场景值',
   PRIMARY KEY (`openid`) USING BTREE,
   INDEX `idx_unionid`(`unionid`) USING BTREE COMMENT 'unionid'
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
