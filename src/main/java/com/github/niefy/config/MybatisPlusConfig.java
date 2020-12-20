9529d7fd0266653a52542940d2b6cd3fef392c1d
Nifury <1549002133@qq.com>
2020-12-20 18:16:09 +0800

    MybatisPlus新分页插件

--- src/main/java/com/github/niefy/config/MybatisPlusConfig.java
+++ src/main/java/com/github/niefy/config/MybatisPlusConfig.java
@@ -1,6 +1,10 @@
 package com.github.niefy.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,14 +13,17 @@ import org.springframework.context.annotation.Configuration;
  * @author Mark sunlightcs@gmail.com
  */
 @Configuration
+@MapperScan("com.github.niefy.modules.*.dao.*")
 public class MybatisPlusConfig {
 
     /**
-     * 分页插件
+     * 分页插件,一缓和二缓遵循mybatis的规则
      */
     @Bean
-    public MybatisPlusInterceptor paginationInterceptor() {
-        return new MybatisPlusInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 
 }
